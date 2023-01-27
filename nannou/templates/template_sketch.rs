dca818405b2d41a2022ce50934f6bfe5a703129b
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-01-27 23:59:39 +0900

    initial commit

---
+++ nannou/templates/template_sketch.rs
@@ -0,0 +1,13 @@
+//cargo watch -x 'run --bin template_sketch'
+use nannou::prelude::*;
+
+fn main() {
+    nannou::sketch(view).run()
+}
+
+fn view(app: &App, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(PLUM);
+    draw.ellipse().color(STEELBLUE);
+    draw.to_frame(app, &frame).unwrap();
+}
